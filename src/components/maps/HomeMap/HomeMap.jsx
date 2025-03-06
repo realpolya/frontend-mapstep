a6292fe598b1236c98f5bc7de643e0e0a2944f0f
realpolya <ps2780@columbia.edu>
2025-03-06 18:22:33 -0500

    map structure

---
+++ src/components/maps/HomeMap/HomeMap.jsx
@@ -0,0 +1,24 @@
+/* --------------------------------Imports--------------------------------*/
+
+import './HomeMap.css'
+
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+/* --------------------------------Variables--------------------------------*/
+
+const MAPBOX_KEY = import.meta.env.VITE_MAPBOX_KEY;
+const mapboxStyle = 'mapbox://styles/polinastepanova/clgsbvw0q001l01qm6uwhceyp/draft'
+
+/* --------------------------------Component--------------------------------*/
+
+
+const HomeMap = () => {
+  return (
+    <div id='div-home-map'>
+        HomeMap
+    </div>
+  )
+}
+
+export default HomeMap
