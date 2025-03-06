89e66e4c8d9feb18696b46eec5a4578629fa2844
realpolya <ps2780@columbia.edu>
2025-03-06 18:35:54 -0500

    add heading mapstep

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,8 +3,9 @@ import HomeMap from "../components/maps/HomeMap/HomeMap.jsx"
 const Home = () => {
   return (
     <main>
-        <HomeMap />
+        <h1>mapStep</h1>
         <p>Lovely day</p>
+        <HomeMap />
     </main>
   )
 }
