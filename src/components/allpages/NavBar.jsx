9db1722fc0ff2419f0113d4d731ab09646196dfa
realpolya <ps2780@columbia.edu>
2025-03-12 09:25:59 -0400

    work on navbar

--- src/components/allpages/NavBar.jsx
+++ src/components/allpages/NavBar.jsx
@@ -5,16 +5,18 @@ import "./allpages.css"
 /* --------------------------------Component--------------------------------*/
 
 const NavBar = () => {
-  return (
-    <nav id="navbar-tw">
-        <h2 className="text-textColor2">mapStep</h2>
+    return (
+        <nav id="navbar-tw">
+        <h2 className="text-textColor2 pl-4 text-2xl">mapStep</h2>
         <div className="nav-searchbar">
-            <label>lot address</label>
-            <input></input>
+            <label className="pr-2">lot address</label>
+            <input className="rounded-xl border-2 border-solid"></input>
         </div>
-        <img id="nav-menu-img" className="h-[50%]" src="/reshot_menu.svg"/>
+        <img id="nav-menu-img" className="h-[50%] pr-4" src="/reshot_menu.svg"/>
     </nav>
   )
 }
+
+/* --------------------------------Default--------------------------------*/
 
 export default NavBar
