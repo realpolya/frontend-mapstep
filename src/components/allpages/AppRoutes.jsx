71f31e5c421365aa2771fd4fbb1d1e0633363529
realpolya <ps2780@columbia.edu>
2025-04-07 14:10:13 -0400

    search result base template

--- src/components/allpages/AppRoutes.jsx
+++ src/components/allpages/AppRoutes.jsx
@@ -3,6 +3,7 @@
 import { Routes, Route } from "react-router-dom"
 
 import Home from "../../pages/Home.jsx"
+import SearchResult from "../../pages/SearchResult.jsx"
 
 /* --------------------------------Component--------------------------------*/
 
@@ -10,6 +11,7 @@ const AppRoutes = () => {
   return (
     <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/result" element={<SearchResult/>} />
     </Routes>
   )
 }
--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,5 +1,9 @@
 /* --------------------------------Imports--------------------------------*/
 
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
 import SearchInfo from "../components/SearchInfo.jsx"
 import SiteMap3D from "../components/maps/SiteMap/SiteMap3D.jsx"
 import SiteMap2D from "../components/maps/SiteMap/SiteMap2D.jsx"
@@ -8,8 +12,40 @@ import VicinityMap from "../components/maps/VicinityMap/VicinityMap.jsx"
 /* --------------------------------Component--------------------------------*/
 
 const SearchResult = () => {
-  return (
-    <div>SearchResult</div>
+
+    let [address, setAddress] = useState('')
+    let location = useLocation()
+
+
+    useEffect(() => {
+
+        if (location.state) {
+            setAddress(location.state.where)
+        }
+
+    }, [location.state])
+
+    
+    return (
+    <div>
+        <h1>Search Results for: [address here]</h1>
+        <div className="div-search-maps">
+            <SiteMap2D />
+            <VicinityMap />
+            <SiteMap3D />
+        </div>
+        <div className="div-search-info">
+            <SearchInfo />
+            <div>
+                <p>to find out the potential and limitations of this property, log in or sign up</p>
+                <div>
+                    <button>Log In</button>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
