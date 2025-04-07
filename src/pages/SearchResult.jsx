850f328037b94b88e48bae160d17cfd9b83c7f51
realpolya <ps2780@columbia.edu>
2025-04-07 16:28:04 -0400

    create context for search page

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,6 +1,6 @@
 /* --------------------------------Imports--------------------------------*/
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, createContext } from 'react';
 import { useLocation } from 'react-router-dom';
 
 
@@ -8,6 +8,17 @@ import SearchInfo from "../components/SearchInfo.jsx"
 import SiteMap3D from "../components/maps/SiteMap/SiteMap3D.jsx"
 import SiteMap2D from "../components/maps/SiteMap/SiteMap2D.jsx"
 import VicinityMap from "../components/maps/VicinityMap/VicinityMap.jsx"
+
+/* --------------------------------Context--------------------------------*/
+
+const SearchContext = createContext(null);
+
+const dummyData = {
+    "latitude": 34.0620051,
+    "longitude": -118.3431566,
+    "lot_width": 280,
+    "lot_depth": 504
+}
 
 /* --------------------------------Component--------------------------------*/
 
@@ -26,26 +37,31 @@ const SearchResult = () => {
     }, [location.state])
 
 
+    const searchObject = { address }
+
     return (
-    <div>
-        <h1>Search Results for: {address}</h1>
-        <div className="div-search-maps">
-            <SiteMap2D />
-            <VicinityMap />
-            <SiteMap3D />
-        </div>
-        <div className="div-search-info">
-            <SearchInfo />
+        <SearchContext.Provider value={searchObject}>
             <div>
-                <p>to find out the potential and limitations of this property, log in or sign up</p>
-                <div>
-                    <button>Log In</button>
-                    <button>Sign Up</button>
+                
+                <h1>Search Results for: {address}</h1>
+                <div className="div-search-maps">
+                    <SiteMap2D />
+                    <VicinityMap />
+                    <SiteMap3D />
                 </div>
+                <div className="div-search-info">
+                    <SearchInfo />
+                    <div>
+                        <p>to find out the potential and limitations of this property, log in or sign up</p>
+                        <div>
+                            <button>Log In</button>
+                            <button>Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
-        </div>
-        
-    </div>
+        </SearchContext.Provider>
   )
 }
 
