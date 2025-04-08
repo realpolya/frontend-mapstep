7dc3f3104b149942acb9450540d2cedb170892bc
realpolya <ps2780@columbia.edu>
2025-04-08 14:21:39 -0400

    create search info box

--- src/components/SearchInfo.jsx
+++ src/components/SearchInfo.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+/* --------------------------------Imports--------------------------------*/
+
+import { useContext, useEffect, useState } from 'react';
+
+import { SearchContext } from '../pages/SearchResult/SearchResult.jsx';
+
+/* --------------------------------Component--------------------------------*/
 
 const SearchInfo = () => {
-  return (
-    <div>SearchInfo</div>
-  )
+
+    const { address, siteDetails } = useContext(SearchContext)
+
+    const [loading, setLoading] = useState(true)
+    const [pageAddress, setPageAddress] = useState('no address yet')
+    const [pageDetails, setPageDetails] = useState('')
+
+    useEffect(() => {
+
+        if (address) setPageAddress(address)
+        if (siteDetails) {
+            setPageDetails(siteDetails)
+            setLoading(false)
+        }
+
+    }, [address, siteDetails])
+
+    return (
+        <div id="div-search-info">
+            <h2>Address: {pageAddress}</h2>
+            <div id="div-search-info-details" className="bg-darkColor text-textAlterColor">
+                { loading ? (<p>No information available yet</p>) : (
+                    <ul>
+                        <li>hi</li>
+                    </ul>
+                )}
+            </div>
+        </div>
+    )
+
 }
+
+/* --------------------------------Export--------------------------------*/
 
 export default SearchInfo
