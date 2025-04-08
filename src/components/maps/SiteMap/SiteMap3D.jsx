503aa3566a96562225070bb6c3f772ccd5d9a39c
realpolya <ps2780@columbia.edu>
2025-04-08 13:24:26 -0400

    3d site map template complete

--- src/components/maps/SiteMap/SiteMap3D.jsx
+++ src/components/maps/SiteMap/SiteMap3D.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+/* --------------------------------Imports--------------------------------*/
+
+import { useContext, useEffect, useState, useRef } from 'react';
+
+import { SearchContext } from '../../../pages/SearchResult.jsx';
+
+import './SiteMap.css'
+import { MAPBOX_KEY, mapboxStyle } from '../variables.js';
+
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+/* --------------------------------Component--------------------------------*/
 
 const SiteMap3D = () => {
-  return (
-    <div>SiteMap3D</div>
-  )
+
+    const { address, siteDetails } = useContext(SearchContext)
+
+    mapboxgl.accessToken = MAPBOX_KEY;
+
+    const [loading, setLoading] = useState(true)
+    const [lat, setLat] = useState(40.7128)
+    const [lng, setLng] = useState(-74.0060)
+
+    const site3DMapRef = useRef(null)
+
+    useEffect(() => {
+
+        if (siteDetails && siteDetails.longitude) {
+            
+            setLat(siteDetails.latitude)
+            setLng(siteDetails.longitude)
+            setLoading(false)
+
+        }
+
+    }, [siteDetails])
+
+
+    useEffect(() => {
+
+        if (!site3DMapRef.current) {
+            console.log('map container is not available at the moment')
+            return;
+        }
+
+        setLoading(false)
+        
+        const map = new mapboxgl.Map({
+            container: site3DMapRef.current,
+            style: mapboxStyle,
+            center: [lng, lat], // starting position [lng, lat]. Note that lat must be set between -90 and 90
+            zoom: 15,
+            pitch: 30,
+            bearing: -17.6
+        });
+
+        return () => map.remove();
+
+    }, [site3DMapRef, MAPBOX_KEY, lng, lat])
+
+    return (
+        <div id='div-site-map-3D'>
+            { loading && (<p>No map yet</p>)}
+
+            <div ref={site3DMapRef} id='site-3d-map-ref'
+            ></div>
+        </div>
+    )
+
 }
+
+/* --------------------------------Export--------------------------------*/
 
 export default SiteMap3D
