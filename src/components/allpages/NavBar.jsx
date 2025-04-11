3238081ede54132b54f23dbf0ca26b1a729f0209
realpolya <ps2780@columbia.edu>
2025-04-11 11:49:50 -0400

    start navbar form for address

--- src/components/allpages/NavBar.jsx
+++ src/components/allpages/NavBar.jsx
@@ -1,19 +1,40 @@
 /* --------------------------------Imports--------------------------------*/
 
-import { Link } from 'react-router-dom';
+import { useState, useEffect, useRef } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 import "./allpages.css"
 
 /* --------------------------------Component--------------------------------*/
 
 const NavBar = () => {
+
+    const navigate = useNavigate()
+
+    const [formAddress, setFormAddress] = useState("")
+
+    const handleWhere = e => setFormAddress(e.target.value)
+    const handleSubmit = e => {
+
+        console.log("form address is ", formAddress)
+        e.preventDefault();
+        navigate("/result", { state: { formAddress }})
+        setFormAddress('')
+
+    }
+
     return (
         <nav id="navbar-tw">
             <Link to="/" className="text-blueColor pl-4 text-2xl">mapStep</Link>
-            <div className="nav-searchbar">
+            <form className="nav-searchbar" onSubmit={handleSubmit}>
                 <label className="pr-2">lot address</label>
-                <input className="rounded-xl border-2 border-solid"></input>
-            </div>
+                <input 
+                    className="rounded-xl border-2 border-solid pl-2"
+                    value={formAddress}
+                    onChange={handleWhere}
+                ></input>
+                <button type="submit" className="hidden">Submit</button>
+            </form>
             <img id="nav-menu-img" className="h-[50%] pr-4" src="/reshot_menu.svg"/>
         </nav>
     )
