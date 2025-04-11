03f36cd783eb0c178ee4246a6ed258f5c3f2907d
realpolya <ps2780@columbia.edu>
2025-04-11 12:15:06 -0400

    introduce loadscript to navbar

--- src/components/allpages/NavBar.jsx
+++ src/components/allpages/NavBar.jsx
@@ -3,7 +3,14 @@
 import { useState, useEffect, useRef } from 'react'
 import { Link, useNavigate } from 'react-router-dom';
 
+// google maps  api
+import { LoadScript, Autocomplete } from "@react-google-maps/api";
+
 import "./allpages.css"
+
+/* --------------------------------Variables--------------------------------*/
+
+const libraries = ['places']
 
 /* --------------------------------Component--------------------------------*/
 
@@ -24,19 +31,24 @@ const NavBar = () => {
     }
 
     return (
-        <nav id="navbar-tw">
-            <Link to="/" className="text-blueColor pl-4 text-2xl">mapStep</Link>
-            <form className="nav-searchbar" onSubmit={handleSubmit}>
-                <label className="pr-2">lot address</label>
-                <input 
-                    className="rounded-xl border-2 border-solid pl-2"
-                    value={formAddress}
-                    onChange={handleWhere}
-                ></input>
-                <button type="submit" className="hidden">Submit</button>
-            </form>
-            <img id="nav-menu-img" className="h-[50%] pr-4" src="/reshot_menu.svg"/>
-        </nav>
+        <LoadScript
+            googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_KEY}
+            libraries={libraries}
+        >
+            <nav id="navbar-tw">
+                <Link to="/" className="text-blueColor pl-4 text-2xl">mapStep</Link>
+                <form className="nav-searchbar" onSubmit={handleSubmit}>
+                    <label className="pr-2">lot address</label>
+                    <input
+                        className="rounded-xl border-2 border-solid pl-2"
+                        value={formAddress}
+                        onChange={handleWhere}
+                    ></input>
+                    <button type="submit" className="hidden">Submit</button>
+                </form>
+                <img id="nav-menu-img" className="h-[50%] pr-4" src="/reshot_menu.svg"/>
+            </nav>
+        </LoadScript>
     )
 }
 
