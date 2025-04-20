87d75c10b5394f6740eb8d359ecc1e6e5fdbdd54
realpolya <ps2780@columbia.edu>
2025-04-20 17:19:25 -0400

    create context, start work on sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
 /* --------------------------------Imports--------------------------------*/
 
+import { createContext } from "react";
+
+
 import './App.css'
 
 import AppRoutes from "./components/allpages/AppRoutes.jsx"
 import NavBar from "./components/allpages/NavBar.jsx"
 import Footer from "./components/allpages/Footer.jsx"
 
+/* --------------------------------Context--------------------------------*/
+
+const AppContext = createContext(null);
+
 /* --------------------------------Component--------------------------------*/
 
 const App = () => {
 
-  return (
-    <>
-        <NavBar/>
-        <AppRoutes/>
-        <Footer/>
-    </>
-  )
+    const appObject = {}
+
+
+
+    return (
+        <AppContext.Provider value={appObject}>
+            <NavBar/>
+            <AppRoutes/>
+            <Footer/>
+        </AppContext.Provider>
+    )
   
 }
 
 /* --------------------------------Default--------------------------------*/
 
 export default App
+export { AppContext }
