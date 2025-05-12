d055faceeaebb066cfa6b21cd7551725f7061d6a
realpolya <ps2780@columbia.edu>
2025-05-12 15:32:59 -0400

    architecture for the dashboard page + useContext

--- src/components/dashboard/AccountInfo.jsx
+++ src/components/dashboard/AccountInfo.jsx
@@ -1,17 +1,21 @@
 /* --------------------------------Imports--------------------------------*/
 
+import { useContext } from "react";
 
+import { AppContext } from "../../App.jsx";
 
 /* --------------------------------Component--------------------------------*/
 
 const AccountInfo = () => {
+
+    const { user } = useContext(AppContext)
 
     return (
         <div>
             AccountInfo
         </div>
     )
-    
+
 }
 
 /* --------------------------------Export--------------------------------*/
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,18 +2,33 @@
 
 import "./Dashboard.css"
 
+import { useContext } from "react";
+
+import { AppContext } from "../../App.jsx";
+
+import AccountInfo from "../../components/dashboard/AccountInfo.jsx";
+import QuickActions from "../../components/dashboard/QuickActions.jsx";
+import RecentProjects from "../../components/dashboard/RecentProjects.jsx";
+
 /* --------------------------------Variables--------------------------------*/
+
 
 
 /* --------------------------------Component--------------------------------*/
 
 const Dashboard = () => {
 
+    const { user } = useContext(AppContext)
+
     return (
         <main>
-            <h1>welcome back, username</h1>
+            <h1>welcome back, <span className="italic">{user.username}</span></h1>
             <div>
-                Dashboard
+                <div>
+                    <RecentProjects/>
+                    <QuickActions/>
+                </div>
+                <AccountInfo/>
             </div>
         </main>
     )
