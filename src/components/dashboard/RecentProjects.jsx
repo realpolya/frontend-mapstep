8b1ddc395fa6e80fdb21e1342a7d4496aee19f16
realpolya <ps2780@columbia.edu>
2025-05-13 11:34:53 -0400

    connect services with the recent projects

--- src/components/dashboard/RecentProjects.jsx
+++ src/components/dashboard/RecentProjects.jsx
@@ -1,14 +1,31 @@
 /* --------------------------------Imports--------------------------------*/
 
+import { useState, useEffect } from "react"
 
+import services from "../../services/index.js"
 
 /* --------------------------------Component--------------------------------*/
 
 const RecentProjects = () => {
 
+    const [myProjects, setMyProjects] = useState([])
+
+    const fetchMyProjects = async () => {
+        const projects = await services.getMyProjects()
+        console.log("projects are ", projects)
+        setMyProjects(await services.getMyProjects())
+    }
+
+    useEffect(() => {
+        fetchMyProjects()
+    }, [])
+
     return (
         <div>
-            RecentProjects
+            <h4>recent projects</h4>
+            {myProjects.map(project => {
+                return <p>{project.address}</p>
+            })}
         </div>
     )
 
