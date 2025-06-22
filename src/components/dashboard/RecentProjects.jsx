b5addd8a717b655de599fb1654a90de923746d17
realpolya <ps2780@columbia.edu>
2025-06-22 08:39:36 -0400

    display the last 3 projects

--- src/components/dashboard/RecentProjects.jsx
+++ src/components/dashboard/RecentProjects.jsx
@@ -12,6 +12,7 @@ import MiniProjectMap from "../maps/MiniProjectMap/MiniProjectMap.jsx"
 const RecentProjects = () => {
 
     const [myProjects, setMyProjects] = useState([])
+    const [myDisplayedProjects, setMyDisplayedProjects] = useState([])
 
     const fetchMyProjects = async () => {
         const projects = await services.getMyProjects()
@@ -23,16 +24,27 @@ const RecentProjects = () => {
         fetchMyProjects()
     }, [])
 
+    useEffect(() => {
+
+        if (myProjects.length > 0) {
+
+            const lastThree = myProjects.slice(-3);
+            setMyDisplayedProjects(lastThree)
+
+        }
+
+    }, [myProjects])
+
     return (
         <div className="pb-4">
             <div className="flex flex-row justify-between pb-4">
                 <h4 className="dash-h4">recent projects</h4>
                 <Link to="/" className="text-blueColor">view all projects</Link>
             </div>
-            <div className="p-2 bg-blueLight rounded-xl">
-                {myProjects.map(project => {
+            <div className="flex flex-row p-2 bg-blueLight rounded-xl">
+                {myDisplayedProjects.map(project => {
                     return (
-                        <Link className="block w-1/3" to={`/project/${project.id}`}>
+                        <Link className="block w-1/3 p-2" to={`/project/${project.id}`}>
                             <MiniProjectMap project={project}/>
                             <p className="italic">{project.title}</p>
                             <p className="text-xs">{project.address}</p>
