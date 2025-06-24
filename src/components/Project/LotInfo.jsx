6c6fa417908f79cf91d53ddf621cbebdba345ff5
realpolya <ps2780@columbia.edu>
2025-06-24 08:49:05 -0400

    table format for lot info

--- src/components/Project/LotInfo.jsx
+++ src/components/Project/LotInfo.jsx
@@ -24,17 +24,43 @@ const LotInfo = () => {
 
     return (
         <div className="mr-4 ml-4 bg-greyColor p-4 text-textAlterColor rounded-xl">
-            <h5>Address: {siteDetails?.address}</h5>
-            <ul className="pt-4">
-                <li className="div-project-li">city: <span className="div-project-span">{siteDetails?.city}</span></li>
-                <li className="div-project-li">jurisdiction: <span className="div-project-span">{siteDetails?.jurisdiction_nested?.name}</span></li>
-                {/* <li className="div-project-li">zoning: <span className="div-project-span">{siteDetails?.zoning}</span></li> */}
-                <li className="div-project-li">year built: <span className="div-project-span">{siteDetails?.year_built}</span></li>
-                <li className="div-project-li">lot width: <span className="div-project-span">{siteDetails?.lot_width} ft</span></li>
-                <li className="div-project-li">lot depth: <span className="div-project-span">{siteDetails?.lot_depth} ft</span></li>
-                <li className="div-project-li">land area (sq. ft): <span className="div-project-span">{siteDetails?.land_area}</span></li>
-                <li className="div-project-li">building area (sq. ft): <span className="div-project-span">{siteDetails?.bldg_area}</span></li>
-            </ul>
+            <h5><span className="font-bold">Address:</span> {siteDetails?.address}</h5>
+
+            <table className="mt-4 w-full">
+                <tr>
+                    <th className="proj-th">state</th>
+                    <td className="proj-td">{siteDetails?.state}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">city</th>
+                    <td className="proj-td">{siteDetails?.city}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">jurisdiction</th>
+                    <td className="proj-td">{siteDetails?.jurisdiction_nested?.name}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">year built</th>
+                    <td className="proj-td">{siteDetails?.year_built}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">lot width (ft)</th>
+                    <td className="proj-td">{siteDetails?.lot_width}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">lot depth (ft)</th>
+                    <td className="proj-td">{siteDetails?.lot_depth}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">land area (sq. ft)</th>
+                    <td className="proj-td">{siteDetails?.land_area}</td>
+                </tr>
+                <tr>
+                    <th className="proj-th">building area (sq. ft)</th>
+                    <td className="proj-td">{siteDetails?.bldg_area}</td>
+                </tr>
+            </table>
+
             <h6 className="pt-4 font-bold">Zonings:</h6>
             <ul className="list-disc pl-5">
                 {zonings.map(zoning => {
@@ -52,3 +78,16 @@ const LotInfo = () => {
 /* --------------------------------Export--------------------------------*/
 
 export default LotInfo
+
+/* --------------------------------List View--------------------------------*/
+
+// <ul className="pt-4">
+//     <li className="div-project-li">city: <span className="div-project-span">{siteDetails?.city}</span></li>
+//     <li className="div-project-li">jurisdiction: <span className="div-project-span">{siteDetails?.jurisdiction_nested?.name}</span></li>
+//     {/* <li className="div-project-li">zoning: <span className="div-project-span">{siteDetails?.zoning}</span></li> */}
+//     <li className="div-project-li">year built: <span className="div-project-span">{siteDetails?.year_built}</span></li>
+//     <li className="div-project-li">lot width: <span className="div-project-span">{siteDetails?.lot_width} ft</span></li>
+//     <li className="div-project-li">lot depth: <span className="div-project-span">{siteDetails?.lot_depth} ft</span></li>
+//     <li className="div-project-li">land area (sq. ft): <span className="div-project-span">{siteDetails?.land_area}</span></li>
+//     <li className="div-project-li">building area (sq. ft): <span className="div-project-span">{siteDetails?.bldg_area}</span></li>
+// </ul>
