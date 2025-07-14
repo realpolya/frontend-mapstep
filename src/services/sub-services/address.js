5dc685c3c5593b43ae71128060b07f6afb97acae
realpolya <ps2780@columbia.edu>
2025-07-14 13:02:32 -0400

    query length for address above 0

--- src/services/sub-services/address.js
+++ src/services/sub-services/address.js
@@ -8,12 +8,14 @@ const getAddress = async (query) => {
 
     try {
 
-        let response = await publicApi.get(`projects/inquiry/?address=${query}`);
-        console.log("response is ", response.data) // locate geometry in an object
-        // console.log("response is ", response.data?.data?.features) // locate geometry in an object
+        if (query && query.length > 0) {
 
+            let response = await publicApi.get(`projects/inquiry/?address=${query}`);
+            console.log("response is ", response.data) // locate geometry in an object
+            // console.log("response is ", response.data?.data?.features) // locate geometry in an object
+            return response.data
 
-        return response.data
+        }
 
     } catch (err) {
 
