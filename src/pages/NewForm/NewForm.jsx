36494b4777ed31ca9ad0336d70dd560f9338f72b
realpolya <ps2780@columbia.edu>
2025-07-31 15:02:13 -0400

    css and newform jsx

--- src/pages/NewForm/NewForm.jsx
+++ src/pages/NewForm/NewForm.jsx
@@ -31,10 +31,40 @@ const NewForm = () => {
     }
     
     return (
-        <main>
-            NewForm
-            <form>
-            
+        <main className="flex flex-col justify-center items-center">
+            <h2 className="text-2xl pb-4">New Project</h2>
+            <form
+            onSubmit={handleSubmit} className="w-full flex flex-col items-center"
+            >
+                <div className="new-form-div">
+                    <label className="new-form-label">
+                        Title:
+                    </label>
+                    <input
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    placeholder=""
+                    className="new-form-input"
+                    />
+                </div>
+                <div className="new-form-div">
+                    <label className="new-form-label">
+                        Address:
+                    </label>
+                    <input
+                    type="text"
+                    name="address"
+                    value={formData.address}
+                    onChange={handleChange}
+                    placeholder=""
+                    className="new-form-input"
+                    />
+                </div>
+                <button type="submit" className="round-button new-form-button">
+                    Save project
+                </button>
             </form>
         </main>
     )
