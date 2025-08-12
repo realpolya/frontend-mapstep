d1acea9193d15a0f3d6c2d1457ae9fa6599dbab4
realpolya <ps2780@columbia.edu>
2025-08-12 19:58:49 -0400

    myProjects.jsx update

--- src/pages/MyProjects/MyProjects.jsx
+++ src/pages/MyProjects/MyProjects.jsx
@@ -1,13 +1,42 @@
 /* --------------------------------Imports--------------------------------*/
 
-import "./MyProjects.css"
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import services from "../../services/index.js";
+
+import "./MyProjects.css";
+
 
 /* --------------------------------Component--------------------------------*/
 
 const MyProjects = () => {
+    
+    const [myProjects, setMyProjects] = useState([])
+    
+    const fetchMyProjects = async () => setMyProjects(await services.getMyProjects())
+
+
+    useEffect(() => {
+
+       fetchMyProjects()
+
+    }, [])
+
 
     return (
-        <div>MyProjects</div>
+        <main>
+            <h2>MyProjects</h2>
+            <div>
+                {myProjects.map(project => {
+                    return (
+                        <Link>
+                            <h6>{project.title}</h6>
+                        </Link>
+                    )
+                })}
+            </div>
+        </main>
     )
 
 }
