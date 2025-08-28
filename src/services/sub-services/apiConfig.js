7ae7907d14b16ab7a65a881baddd97919572bc3d
realpolya <ps2780@columbia.edu>
2025-08-28 18:37:19 -0400

    revise services with cookies http

--- src/services/sub-services/apiConfig.js
+++ src/services/sub-services/apiConfig.js
@@ -8,38 +8,48 @@ const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
 
 /* --------------------------------Functions--------------------------------*/
 
-const getToken = () => {
+// const getToken = () => {
 
-    const token = localStorage.getItem('token');
-    if (!token) return null;
-    return `Bearer ${token}`;
+//     const token = localStorage.getItem('token');
+//     if (!token) return null;
+//     return `Bearer ${token}`;
     
-};
+// };
+
+// const api = axios.create({
+//     baseURL: BACKEND_URL
+// });
+
+// const publicApi = axios.create({
+//     baseURL: BACKEND_URL
+// });
 
 const api = axios.create({
-    baseURL: BACKEND_URL
+    baseURL: BACKEND_URL,
+    withCredentials: true,
 });
 
 const publicApi = axios.create({
-    baseURL: BACKEND_URL
+    baseURL: BACKEND_URL,
+    withCredentials: true,
 });
 
-api.interceptors.request.use(
-    function (config) {
+// api.interceptors.request.use(
+//     function (config) {
 
-        const token = getToken();
-        if (token) {
-            config.headers["Authorization"] = token;
-        }
+//         const token = getToken();
+//         if (token) {
+//             config.headers["Authorization"] = token;
+//         }
 
-        return config;
-    },
+//         return config;
+//     },
     
-    function (error) {
-        console.log("Request error: ", error);
-        return Promise.reject(error);
-    }
-);
+//     function (error) {
+//         console.log("Request error: ", error);
+//         return Promise.reject(error);
+//     }
+// );
 
 /* --------------------------------Exports--------------------------------*/
 
--- src/services/sub-services/auth.js
+++ src/services/sub-services/auth.js
@@ -9,7 +9,7 @@ const signUp = async (formData) => {
     
     try {
         const response = await publicApi.post("users/register/", formData);
-        localStorage.setItem("token", response.data.access);
+        // localStorage.setItem("token", response.data.access);
         return response.data.user;
     } catch (err) {
         console.log(err.response.data.error);
@@ -22,7 +22,7 @@ const signIn = async (formData) => {
 
     try {
         const response = await publicApi.post("users/login/", formData);
-        localStorage.setItem("token", response.data.access);
+        // localStorage.setItem("token", response.data.access);
         return response.data.user;
     } catch (err) {
         console.log(err.response.data.error);
@@ -32,38 +32,78 @@ const signIn = async (formData) => {
 };
 
 // local getUser from the localStorage
-const getUser = () => {
-
-    const token = localStorage.getItem("token");
-    if (!token) return null;
-    return JSON.parse(atob(token.split(".")[1]));;
-
-};
-
-// call the API to verify token
-const verifyToken = async () => {
-
-    if (localStorage.getItem("token")) {
-        const response = await api.get("users/verify/");
-        localStorage.setItem("token", response.data.access);
-        return response.data.user;
-    }
-
-    console.log("No user");
-    return false;
-};
-
-const signOut = () => {
+const getUser = async () => {
 
     try {
-        localStorage.removeItem("token");
-        console.log("Signed out");
-    } catch (error) {
+        const response = await api.get("users/me/");
+        return response.data.user;
+    } catch (err) {
         console.log(err.response.data.error);
         throw err;
     }
 
+    // const token = localStorage.getItem("token");
+    // if (!token) return null;
+    // return JSON.parse(atob(token.split(".")[1]));;
+
 };
+
+// call the API to verify token
+// const verifyToken = async () => {
+
+//     if (localStorage.getItem("token")) {
+//         const response = await api.get("users/verify/");
+//         localStorage.setItem("token", response.data.access);
+//         return response.data.user;
+//     }
+
+//     console.log("No user");
+//     return false;
+// };
+
+const verifySession = async () => {
+
+    try {
+
+        const response = await api.post("users/verify/");
+        return response?.data;
+
+    } catch (err) {
+
+        console.log(err.response.data.error);
+        throw err;
+
+    }
+
+}
+
+// const signOut = () => {
+
+//     try {
+//         localStorage.removeItem("token");
+//         console.log("Signed out");
+//     } catch (error) {
+//         console.log(err.response.data.error);
+//         throw err;
+//     }
+
+// };
+
+const signOut = async () => {
+
+    try {
+
+        const response = await api.post("users/verify/");
+        return response?.data;
+
+    } catch (err) {
+
+        console.log(err.response.data.error);
+        throw err;
+        
+    }
+
+}
 
 /* --------------------------------Exports--------------------------------*/
 
