be2d8ca555187b5846fb4df4836fac2140bf28f0
realpolya <ps2780@columbia.edu>
2025-09-08 13:43:27 -0400

    fix arrow function, now need text greyed out when clicking is done

--- src/pages/MyProjects/MyProjects.jsx
+++ src/pages/MyProjects/MyProjects.jsx
@@ -15,16 +15,15 @@ const limit = 4 // limit of projects per page
 /* --------------------------------Component--------------------------------*/
 
 const MyProjects = () => {
-
-    // TODO: 8 projects per page
     
     const [myProjects, setMyProjects] = useState([])
     const [loading, setLoading] = useState(true)
     const [displayedProjects, setDisplayedProjects] = useState([])
-    const [count, setCount] = useState([0, 7]) // indices of the projects
+    const [count, setCount] = useState([0, limit - 1]) // initial indices of the projects
     const [maxCount, setMaxCount] = useState(0)
     const [maxPages, setMaxPages] = useState(1)
     
+
     const fetchMyProjects = async () => {
 
         const fetched = await services.getMyProjects()
@@ -32,7 +31,32 @@ const MyProjects = () => {
 
     }
 
+    const clickNext = () => {
 
+        // if reached the end
+        if ((count[0] + limit) > maxCount) return;
+
+        const prevStart = count[0]
+        const prevEnd = count[1]
+        
+        // move the indices
+        setCount([prevStart + limit, prevEnd + limit])
+        return; 
+        
+    }
+
+    const clickPrevious = () => {
+
+        if ((count[1] - limit) < 0) return;
+
+        const prevStart = count[0]
+        const prevEnd = count[1]
+        
+        // move the indices
+        setCount([prevStart - limit, prevEnd - limit])
+        return; 
+
+    }
 
 
     useEffect(() => {
@@ -47,7 +71,7 @@ const MyProjects = () => {
         if (myProjects.length > 0) {
 
             const projectsNum = myProjects.length - 1
-            setMaxCount(projectsNum)
+            setMaxCount(projectsNum) // index is 1 fewer
             setMaxPages(Math.floor(projectsNum/limit))
 
             setLoading(false)
@@ -59,21 +83,29 @@ const MyProjects = () => {
 
     useEffect(() => {
 
+        console.log("firing now here, loading is ", loading)
+
         if (!loading) {
 
             const currentProj = []
             let i = count[0]
 
             for (let c = 0; c < limit; c++) {
+
+                if (i > maxCount) break;
+
                 currentProj.push(myProjects[i]);
                 i++;
+
             }
+
+            console.log("firing now")
 
             setDisplayedProjects(currentProj)
 
         }
 
-    }, [loading]) // add arrows to the dependency array
+    }, [loading, count]) // add arrows to the dependency array
 
 
 
@@ -85,9 +117,9 @@ const MyProjects = () => {
                     <Link to="/dashboard" className="red-link text-center">back to dashboard</Link>
                 </div>
                 <div className="my-projects-arrows">
-                    <button className="arrows-button">⬅️ Previous&nbsp;</button>
+                    <button className="arrows-button" onClick={clickPrevious}>⬅️ Previous&nbsp;</button>
                     <p> | | </p>
-                    <button className="arrows-button">&nbsp;Next ➡️</button>
+                    <button className="arrows-button" onClick={clickNext}>&nbsp;Next ➡️</button>
                 </div>
             </div>
             <div className="grid grid-cols-2 grid-rows-2 gap-4">
