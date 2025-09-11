8b897c9604483c8234ea516649000168ea601cbd
realpolya <ps2780@columbia.edu>
2025-09-11 13:54:20 -0400

    organize the about page, add images, short paragraphs

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,5 +1,7 @@
 /* --------------------------------Imports--------------------------------*/
 
+import { Link } from 'react-router-dom'
+
 import "./About.css"
 
 /* --------------------------------Component--------------------------------*/
@@ -8,7 +10,24 @@ const About = () => {
 
     return (
 
-        <main>About</main>
+        <main className="normal-main flex flex-col items-center">
+            <h1 className="h1-heading">About mapStep</h1>
+            <div className="flex flex-col w-full items-center">
+                <div className="about-text-img-div">
+                    <p><span className="font-medium">mapStep</span> was created
+                        by <a className="red-link" href="https://www.linkedin.com/in/realpolya/">Polina Stepanova</a> as
+                        an efficiency and organization tool for architects,
+                        real estate developers, contractors, civil engineers, and urban planners.
+                    </p>
+                    <img src="/polina.jpg" className="w-[200px] rounded-full"/>
+                </div>
+                <div className="about-text-img-div">
+                    <img src="/sketches.png" className="w-[400px] rounded-full"/>
+                    <p>lovely</p>
+                </div>
+            </div>
+
+        </main>
 
     )
 }
