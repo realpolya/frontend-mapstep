b2f30e2834def36a6149229ded35112c017e63f3
realpolya <ps2780@columbia.edu>
2025-09-11 14:24:51 -0400

    work on about page for mapstep

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -11,19 +11,40 @@ const About = () => {
     return (
 
         <main className="normal-main flex flex-col items-center">
-            <h1 className="h1-heading">About mapStep</h1>
+            <h1 className="h1-heading">about mapStep</h1>
             <div className="flex flex-col w-full items-center">
                 <div className="about-text-img-div">
-                    <p><span className="font-medium">mapStep</span> was created
+                    <p className="pr-4"><span className="font-medium">mapStep</span> was created
                         by <a className="red-link" href="https://www.linkedin.com/in/realpolya/">Polina Stepanova</a> as
                         an efficiency and organization tool for architects,
-                        real estate developers, contractors, civil engineers, and urban planners.
+                        real estate developers, contractors, civil engineers, and urban planners.<br/>
+                        <br/>
+                        We strive to support creating better architecture with clarity, speed, and creativity. We want to maximize productivity
+                        and increase ROI from each development project.
                     </p>
                     <img src="/polina.jpg" className="w-[200px] rounded-full"/>
                 </div>
                 <div className="about-text-img-div">
                     <img src="/sketches.png" className="w-[400px] rounded-full"/>
-                    <p>lovely</p>
+                    <p className="pl-4">The application intends to serve as a centralized database for obtaining
+                        zoning, planning, and building code information for each individual 
+                        plot of land across the United States. The building code information 
+                        has been unneccesarily bureaucratic and disjointed – mapStep was created 
+                        out of an aspiration to fix the system and streamline the design and construction-related
+                        processes.
+                    </p>
+                </div>
+                <div className="about-text-img-div">
+                    <p className="pr-4">Based on Los Angeles as a sample city and utilizing 
+                        public <a className="red-link" href="https://portal.assessor.lacounty.gov/">Los Angeles County Assessor's portal</a> as 
+                        the primary API endpoint, mapStep enhances the permit acquisition process and broadens the 
+                        imagination horizon by suggesting construction possibilities. The lot research
+                        is significantly augmented and explanded with AI technologies.
+                        <br/>
+                        <br/>
+                        contact us for inquiries
+                    </p>
+                    <img src="/sketch.jpg" className="w-[200px] rounded-full"/>
                 </div>
             </div>
 
