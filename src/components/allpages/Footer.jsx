d49f61a05e055028438a5dc687c958dea6c045db
realpolya <ps2780@columbia.edu>
2025-09-11 17:32:35 -0400

    fix link to FAQ

--- src/components/allpages/Footer.jsx
+++ src/components/allpages/Footer.jsx
@@ -5,15 +5,19 @@ import { Link } from "react-router-dom";
 /* --------------------------------Component--------------------------------*/
 
 const Footer = () => {
-  return (
-    <footer>
-        <p className="pl-4">mapStep © 2025 all rights reserved</p>
-        <div id="footer-links" className="pr-4">
-            <Link className="pr-2" to="/about">About</Link>
-            <a href="https://github.com/realpolya">Creator</a>
-        </div>
-    </footer>
-  )
+
+    return (
+        <footer>
+            {/* <p className="pl-4">mapStep © 2025 all rights reserved</p> */}
+            <p className="pl-4">mapStep 2025</p>
+            <div id="footer-links" className="pr-4">
+                <Link className="pr-2" to="/faq">FAQ</Link>
+                <Link className="pr-2" to="/about">About</Link>
+                <a href="https://github.com/realpolya">Creator</a>
+            </div>
+        </footer>
+    )
+
 }
 
 /* --------------------------------Export--------------------------------*/
--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -1,16 +1,27 @@
 /* --------------------------------Imports--------------------------------*/
 
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
 import './FAQ.css'
+
+import { AppContext } from '../../App.jsx'
 
 /* --------------------------------Component--------------------------------*/
 
 const FAQ = () => {
 
+    const { user } = useContext(AppContext)
+
     return (
 
-        <div>
-            FAQ
-        </div>
+        <main>
+            <div>
+                <h1>FAQ</h1>
+                { user ? (<Link to="/dashboard">to dashboard</Link>) : (null) }
+            </div>
+
+        </main>
 
     )
 
