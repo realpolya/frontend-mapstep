8143948b11511537a9929e5144bae0b451c6d750
realpolya <ps2780@columbia.edu>
2025-10-02 14:01:15 -0400

    init placeholder

---
+++ src/pages/Placeholder/Placeholder.jsx
@@ -0,0 +1,21 @@
+/* --------------------------------Imports--------------------------------*/
+
+import { Link } from 'react-router-dom';
+
+import "./Placeholder.css";
+
+/* --------------------------------Component--------------------------------*/
+
+const Placeholder = () => {
+
+
+
+    return (
+        <main>Placeholder</main>
+    )
+
+}
+
+/* --------------------------------Export--------------------------------*/
+
+export default Placeholder
