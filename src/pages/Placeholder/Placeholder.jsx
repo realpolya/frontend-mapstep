ffe30e79cc9ab143140bbf83fac944c8aa1f471c
realpolya <ps2780@columbia.edu>
2025-10-02 14:31:14 -0400

    css for placeholder & navbar

--- src/pages/Placeholder/Placeholder.jsx
+++ src/pages/Placeholder/Placeholder.jsx
@@ -16,13 +16,18 @@ const Placeholder = () => {
 
 
     return (
-        <main>
-            <h2>Hi there!</h2>
-            <p>This little corner of mapStep has not been developed yet!</p>
-            <p>Please check back in soon :)</p>
-            <p>In the meantime:</p>
+        <main className="placeholder-main">
+            <div className="flex flex-col sm:flex-row w-full items-center justify-between pb-12">
+                <div className="sm:w-1/2">
+                    <h1 className="h1-heading">Hi there!</h1>
+                    <p>This little corner of <span className="text-blueColor">mapStep</span> has not been developed yet!</p>
+                    <p className="pb-12">Please check back in soon :)</p>
+                </div>
+                <img src="/logo.png" className="w-60 h-60 mr-2 mt-1"/>
+            </div>
+            <p>In the meantime...</p>
             <div>
-                <button className="round-button">Go to home page</button>
+                <button className="round-button red-button">Go to home page</button>
                 <button className="round-button">Learn about mapStep</button>
                 { user ? (<button className="round-button">Go to dashboard</button>) : (null) }
             </div>
