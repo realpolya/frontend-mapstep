7d6f9e13eee37cca8bffcb0ebe701f4d0f3b254c
realpolya <ps2780@columbia.edu>
2025-10-06 13:10:30 -0400

    start project context provider

---
+++ src/providers/ProjectProvider.jsx
@@ -0,0 +1,20 @@
+/* --------------------------------Imports--------------------------------*/
+
+import { createContext, useEffect } from 'react';
+
+import services from '../services/index.js';
+
+/* --------------------------------Component--------------------------------*/
+
+
+const ProjectProvider = () => {
+
+    return (
+        <div>ProjectProvider</div>
+    )
+    
+}
+
+/* --------------------------------Export--------------------------------*/
+
+export default ProjectProvider
