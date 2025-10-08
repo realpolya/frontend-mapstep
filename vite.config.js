96ce3ed9e51dc49be71206a5d6b4395b9079a80d
realpolya <ps2780@columbia.edu>
2025-10-08 10:22:36 -0400

    change vite config file for production

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,42 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [
-        react(),
-    ],
-    server: {
+// // https://vite.dev/config/
+// export default defineConfig({
+//     plugins: [
+//         react(),
+//     ],
+//     server: {
+//         https: {
+//         key: fs.readFileSync('./key.pem'),
+//         cert: fs.readFileSync('./cert.pem'),
+//         },
+//         host: true,
+//         port: 5173,
+//   },
+// })
+
+
+export default defineConfig(({ command }) => {
+  // When running `vite serve` locally - using key pem for https
+  if (command === 'serve') {
+    return {
+      plugins: [react()],
+      server: {
         https: {
-        key: fs.readFileSync('./key.pem'),
-        cert: fs.readFileSync('./cert.pem'),
+          key: fs.readFileSync('./key.pem'),
+          cert: fs.readFileSync('./cert.pem'),
         },
         host: true,
         port: 5173,
-  },
+      },
+    }
+  } 
+  
+  // netlify production
+  else {
+    return {
+      plugins: [react()],
+    }
+  }
 })
