822d55541572c9df9baec0892559f40690bba5f2
realpolya <ps2780@columbia.edu>
2025-10-08 17:01:02 -0400

    deploy with new faq

--- src/pages/FAQ/FAQ.jsx
+++ src/pages/FAQ/FAQ.jsx
@@ -9,7 +9,7 @@ import { AppContext } from '../../App.jsx'
 
 /* --------------------------------Varibles--------------------------------*/
 
-const Q_NUM = 1
+const Q_NUM = 2
 
 /* --------------------------------Component--------------------------------*/
 
@@ -62,6 +62,10 @@ const FAQ = () => {
                 <div id="div-faq-2" className="faq-dropdown-div">
                     <button className="faq-dropdown-btn" onClick={() => {updateBool(2)}}>↓ Who is mapStep designed for?</button>
                     { qArray[2] ? (<p className="faq-answer-p">Architects, contractors, civil engineers, designers, real estate developers and investors, opportunists, and homeowners. Practically anyone who is interacting with the world of built environment.</p>) : (null) }
+                </div>
+                <div id="div-faq-2" className="faq-dropdown-div">
+                    <button className="faq-dropdown-btn" onClick={() => {updateBool(2)}}>↓ How was mapstep created?</button>
+                    { qArray[2] ? (<p className="faq-answer-p">An architectural designer realized that technology can significantly optimize existing bureaucratic processes within architecture and construction industries.</p>) : (null) }
                 </div>
             </div>
         </main>
